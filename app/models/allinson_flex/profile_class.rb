670f74cba61c7365e665e00722f7b57093bad08a
Daniel Pierce <dlpierce@indiana.edu>
2022-07-11 16:52:43 -0400

    Avoid n+1 query via available_properties

--- app/models/allinson_flex/profile_class.rb
+++ app/models/allinson_flex/profile_class.rb
@@ -5,7 +5,7 @@ module AllinsonFlex
     # @todo any before_destroy validation?
     # many to many relationship between class and context
     has_and_belongs_to_many :profile_contexts
-    has_many :available_properties, as: :available_on, class_name: 'AllinsonFlex::ProfileAvailableProperty', dependent: :destroy
+    has_many :available_properties, -> { includes(profile_property: :texts) }, as: :available_on, class_name: 'AllinsonFlex::ProfileAvailableProperty', dependent: :destroy
     has_many :properties, through: :available_properties, source: :profile_property
     has_many :class_texts, as: :textable, class_name: 'AllinsonFlex::ProfileText', dependent: :destroy
     accepts_nested_attributes_for :profile_contexts, :class_texts
