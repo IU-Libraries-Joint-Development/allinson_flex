4f0092eb83a4e3e98538636febef9f3b37b01546
Daniel Pierce <dlpierce@indiana.edu>
2022-07-11 12:09:27 -0400

    Avoid n+1 query via profile_available_property

--- app/models/allinson_flex/profile_available_property.rb
+++ app/models/allinson_flex/profile_available_property.rb
@@ -2,7 +2,7 @@
 
 module AllinsonFlex
   class ProfileAvailableProperty < ApplicationRecord
-    belongs_to :profile_property, required: false
+    belongs_to :profile_property, -> { includes(:texts) }, required: false
     belongs_to :available_on, polymorphic: true, required: false
   end
 end
